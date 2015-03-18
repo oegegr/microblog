8499a88f67f353c00cd2a8b7f901fbd8e2087708
Georgy Aleksandrov <aleksandrov@Aleksandrov-Mac-mini.local>
2015-03-18 14:14:29 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+WTF_CRF_ENABLED = True
+SECRET_KEY = '1qaz2wsx'
+
+#mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+#admins list
+ADMINS = ['aleksandrovgn@gmail.com']
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'},
+    {'name': 'LiveJournal', 'url': 'http://<username>.livejournal.com'}]
